bfd3caa3b80dba8966db0383228fd3d0a54c733f
Ismael Rumzan <ismael.rumzan@gmail.com>
2021-07-27 12:24:21 +0300

    added vercel domain

--- next.config.js
+++ next.config.js
@@ -10,7 +10,17 @@ module.exports = {
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ]
-      }
+      },
+      {
+        // matching all API routes
+        source: "/(.*)",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://font-load-cors-nextjs.vercel.app" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }      
     ]
   }
 };
